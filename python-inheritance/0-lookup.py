9edabdd710f759a1f532894cfc400820ec896710
Raphael Henry Musoke <r.musoke@alustudent.com>
2023-11-23 15:35:43 -0800

    all

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,9 @@
-No
+#!/usr/bin/python3
+'''Function returns a list of all object attributes and methods
+'''
+
+
+def lookup(obj):
+    '''Returns all the methods and attributes
+    '''
+    return dir(obj)
