cb0890b1b86ce3e4171a40867f5e97d210f508c4
Raphael Henry Musoke <r.musoke@alustudent.com>
2023-11-27 11:42:34 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-'''Defines function called write_file
-'''
+write_file = __import__('1-write_file').write_file
 
-
-def write_file(filename="", text=""):
-    '''Writes text to filename
-
-    Args:
-       filename (str): The directory to the file being modified
-       text (str): The string written into filename.
-    '''
-    with open(filename, 'w', encoding="utf-8") as f:
-        size = f.write(text)
-    return sizie
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
